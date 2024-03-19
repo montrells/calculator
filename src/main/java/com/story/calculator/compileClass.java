108359da60e91e15536bce6d2ff6647e7737479a
montrells <montrell.story@gmail.com>
2024-03-19 14:47:49 +0300

    testing compileJava

--- src/main/java/com/story/calculator/compileClass.java
+++ src/main/java/com/story/calculator/compileClass.java
@@ -1,7 +1,7 @@
-package com.story.calculator;
-
-public class compileClass {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
+//package com.story.calculator;
+//
+//public class compileClass {
+//    public static void main(String[] args) {
+//        System.out.println("Hello World");
+//    }
+//}
