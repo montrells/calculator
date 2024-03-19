2da9497e30aa7b504938bef5667b4c061b52356f
montrells <montrell.story@gmail.com>
2024-03-19 14:34:26 +0300

    testing compileJava

--- src/main/java/com/story/calculator/Calculator.java
+++ src/main/java/com/story/calculator/Calculator.java
@@ -1,13 +1,13 @@
-package com.story.calculator;
-
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.stereotype.Service;
-
-/** Calculator logic */
-@Service
-public class Calculator {
-	@Cacheable("sum")
-	public int sum(int a, int b) {
-		return a + b;
-	}
-}
+//package com.story.calculator;
+//
+//import org.springframework.cache.annotation.Cacheable;
+//import org.springframework.stereotype.Service;
+//
+///** Calculator logic */
+//@Service
+//public class Calculator {
+//	@Cacheable("sum")
+//	public int sum(int a, int b) {
+//		return a + b;
+//	}
+//}
--- src/main/java/com/story/calculator/compileClass.java
+++ src/main/java/com/story/calculator/compileClass.java
@@ -1,8 +1,7 @@
 package com.story.calculator;
 
-public class compileJava {
+public class compileClass {
     public static void main(String[] args) {
-        System.out.println("Hello world!!!");
+        System.out.println("Hello World");
     }
-
 }
